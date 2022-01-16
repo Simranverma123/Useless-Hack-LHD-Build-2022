eaa9fc6dd92629beabe8fb49b17e45b245311cd8
Simran Verma <88669720+Simranverma123@users.noreply.github.com>
2022-01-16 14:12:33 +0530

    Program added

---
+++ useless.cpp
@@ -0,0 +1,16 @@
+//useless hack
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"Enter Any Number:"<<endl;
+cin>>n;
+while(true)
+{
+int a=rand();
+cout<<a;
+}
+return 0;
+}
